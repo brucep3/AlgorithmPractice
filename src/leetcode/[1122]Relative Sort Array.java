13c76bd878f3d57f83e3d18d6ddc012cc0cc91f4
Bruce Pan <brucepan3@163.com>
2020-11-14 14:36:09 +0800

    设置版本控制文件

---
+++ src/leetcode/[1122]Relative Sort Array.java
@@ -0,0 +1,40 @@
+package leetcode;
+
+class RelativeSortArray {
+
+
+}
+
+//Given two arrays arr1 and arr2, the elements of arr2 are distinct, and all ele
+//ments in arr2 are also in arr1.
+//
+// Sort the elements of arr1 such that the relative ordering of items in arr1 ar
+//e the same as in arr2. Elements that don't appear in arr2 should be placed at th
+//e end of arr1 in ascending order.
+//
+//
+// Example 1:
+// Input: arr1 = [2,3,1,3,2,4,6,7,9,2,19], arr2 = [2,1,4,3,9,6]
+//Output: [2,2,2,1,4,3,3,9,6,7,19]
+//
+//
+// Constraints:
+//
+//
+// arr1.length, arr2.length <= 1000
+// 0 <= arr1[i], arr2[i] <= 1000
+// Each arr2[i] is distinct.
+// Each arr2[i] is in arr1.
+//
+// Related Topics 排序 数组
+// 👍 104 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        
+        return new int[1];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
