4ce7002a42c34ca4c4e14c33bef84c76b38aded9
Bruce Pan <brucepan3@163.com>
2021-05-04 00:34:10 +0800

    2021.05.03 submit

---
+++ java/src/weeklycontest/Solution.java
@@ -0,0 +1,55 @@
+package weeklycontest;
+
+public class Solution {
+    public int getMinSwaps(String num, int k) {
+        return 0;
+    }
+
+    public void nextPermutation(int[] num) {
+        int n = num.length;
+        if (n < 2) {
+            return;
+        }
+        int index = n - 1;
+        //从末尾开始寻找第一个不满足降序的元素 （124653 中找4）
+        while (index > 0) {
+            if (num[index - 1] < num[index]) {
+                break;
+            }
+            index--;
+        }
+        if (index == 0) {
+            reverseSort(num, 0, n - 1);
+            return;
+        }
+        int val = num[index - 1];
+        int j = n - 1;
+        //从末尾开始寻找第一个大于已经在第一步中找到的那个元素索引（124653 中找5）
+        while (j >= index) {
+            if (num[j] > val) {
+                break;
+            }
+            j--;
+        }
+        //交换这两个元素位置
+        swap(num, j, index - 1);
+        //逆置之后的元素
+        reverseSort(num, index, n - 1);
+    }
+
+    public void swap(int[] num, int i, int j) {
+        int temp = 0;
+        temp = num[i];
+        num[i] = num[j];
+        num[j] = temp;
+    }
+
+    public void reverseSort(int[] num, int start, int end) {
+        if (start > end) {
+            return;
+        }
+        for (int i = start; i <= (end + start) / 2; i++) {
+            swap(num, i, start + end - i);
+        }
+    }
+}
--- js/leetcode_weekly_contest/weekly_contest_used_2.js
+++ js/leetcode_weekly_contest/weekly_contest_used_2.js
@@ -1,95 +1,30 @@
 /**
- * @param {number} n
+ * @param {string} s
+ * @return {boolean}
  */
-var SeatManager = function (n) {
-    // 构建一个小顶堆
-    let heap = [,], i = 1;
-    while (i <= n) {
-        heap.push(i);
-    }
-    buildHeap(heap, n);
-    this.minHeap = heap;
-    this.size = n;
-};
+var splitString = function (s) {
 
-/**
- * @return {number}
- */
-SeatManager.prototype.reserve = function () {
+    const helper = (s, target) => {
+        if (+s === target) return true;
+        for (let i = 1; i <= s.length; ++i) {
+            if (+s.substr(0, i) === target) {
+                return helper(s.substr(i), target - 1);
+            } else if (+s.substr(0, i) > target) {
+                return false;
+            }
+        }
+        return false;
+    };
 
-};
-
-/**
- * @param {number} seatNumber
- * @return {void}
- */
-SeatManager.prototype.unreserve = function (seatNumber) {
-    this.minHeap[seatNumber] = false;
-};
-
-let findKthLargest = function (nums, k) {
-    // 从 nums 中取出前 k 个数，构建一个小顶堆
-    let heap = [,], i = 0
-    while (i < k) {
-        heap.push(nums[i++])
-    }
-    buildHeap(heap, k)
-
-    // 从 k 位开始遍历数组
-    for (let i = k; i < nums.length; i++) {
-        if (heap[1] < nums[i]) {
-            // 替换并堆化
-            heap[1] = nums[i]
-            heapify(heap, k, 1)
+    for (let i = 1; i < s.length; ++i) {
+        let cur = +s.substr(0, i);
+        if (cur > 0) {
+            if (helper(s.substr(i), cur-1)) return true;
         }
     }
-
-    // 返回堆顶元素
-    return heap[1]
+    return false;
 };
 
-// 原地建堆，从后往前，自上而下式建小顶堆
-let buildHeap = (arr, k) => {
-    if (k === 1) return
-    // 从最后一个非叶子节点开始，自上而下式堆化
-    for (let i = Math.floor(k / 2); i >= 1; i--) {
-        heapify(arr, k, i);
-    }
-}
+// console.log(splitString("1234"));
+// console.log(splitString("050043"));
 
-/**
- * 堆化
- * @param arr
- * @param k
- * @param i
- */
-let heapify = (arr, k, i) => {
-    // 自上而下式堆化
-    while (true) {
-        let minIndex = i
-        if (2 * i <= k && arr[2 * i] < arr[i]) {
-            minIndex = 2 * i
-        }
-        if (2 * i + 1 <= k && arr[2 * i + 1] < arr[minIndex]) {
-            minIndex = 2 * i + 1
-        }
-        if (minIndex !== i) {
-            swap(arr, i, minIndex)
-            i = minIndex
-        } else {
-            break
-        }
-    }
-}
-
-/**
- * 交换
- * @param arr
- * @param i
- * @param j
- */
-let swap = (arr, i, j) => {
-    let temp = arr[i]
-    arr[i] = arr[j]
-    arr[j] = temp
-}
