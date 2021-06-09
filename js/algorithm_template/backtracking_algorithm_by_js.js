cd189e78ed5b6b1c342d9a4c59220bb27fcf11a2
Bruce Pan <brucepan3@163.com>
2021-06-09 14:12:18 +0800

    2021.06.09 update

--- js/algorithm_template/backtracking_algorithm_by_js.js
+++ js/algorithm_template/backtracking_algorithm_by_js.js
@@ -4,10 +4,10 @@
  * 回溯算法
  * 0/13道题
  * Subsets I, II(lc-78, 90),
- * Permutations I, II(lc-46, 47)
+ * Permutations I, II(lc-46, 47),
  * Combination Sum I, II, III, IV(lc-39, 40, 216, 377),
- * Palindrome Partitioning I, II(lc-131, 132)
- * Word Search I, II(lc-79, 212)
+ * Palindrome Partitioning I, II(lc-131, 132),
+ * Word Search I, II(lc-79, 212),
  * Restore IP Addresses(lc-93)
  */
 
--- js/leetcode/powx-n.js
+++ js/leetcode/powx-n.js
@@ -5,25 +5,16 @@
  * @return {number}
  */
 var myPow = function (x, n) {
-    // powMap.val = x ^ ( powMap.key - 1)
-    let powMap = new Map().set(0, x),
-        sign = Math.sign(n),
-        binN = Math.abs(n).toString(2).split("").reverse().join("");
-
-    for (let i = 1, powRes = x; i < binN.length + 1; ++i) {
-        powRes = powRes * powRes;
-        powMap.set(i, powRes);
-    }
+    let sign = Math.sign(n);
+    n = Math.abs(n);
 
     let res = 1;
-    for (let i = 0; i < binN.length; ++i) {
-        if (binN.charAt(i) === "1") {
-            res = res * powMap.get(i);
-        }
+
+    // 注意事项：使用 >>>= 无符号右移，而不是有符号右移
+    for (let base = x, curN = n; curN; base *= base, curN >>>= 1) if (curN & 1 === 1) {
+        res *= base;
     }
 
     if (sign > 0) return res;
     else return 1 / res;
 };
-
-// myPow(2.00000, 10);
