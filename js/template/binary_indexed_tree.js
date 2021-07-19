6536ab774dc1cc1a9a8e2c778fbbce7c110e917d
Bruce Pan <brucepan3@163.com>
2021-07-19 20:16:23 +0800

    lc-307. Range Sum Query - Mutable (307. 区域和检索 - 数组可修改)

--- js/template/binary_indexed_tree.js
+++ js/template/binary_indexed_tree.js
@@ -4,6 +4,9 @@
  * @Date 2021/7/19
  */
 
+/**
+ * 树状数组
+ */
 class BinaryIndexedTree {
 
     /**
@@ -27,7 +30,7 @@ class BinaryIndexedTree {
 
     /**
      * 查询前缀和
-     * sum[0 : x] or sum[1 : x + 1]
+     * sum[1, x]
      * @param x
      */
     sum(x) {
@@ -48,6 +51,25 @@ class BinaryIndexedTree {
         return this.sum(r) - this.sum(l - 1);
     }
 
+    /**
+     * 通过数组初始化树状数组
+     * 确保树状数组长度足够
+     * @param arr
+     */
+    init(arr) {
+        const preSum = [0];
+        for (const ele of arr) preSum.push(preSum[preSum.length - 1] + ele);
+        // (i - lowbit(i), i]
+        for (let i = 1; i < this.tr.length; ++i) {
+            this.tr[i] = preSum[i] - preSum[i - this.constructor.lowbit(i)];
+        }
+    }
+
+    /**
+     * 返回 x 的最后一个 1 和 1 右边的 0
+     * @param x
+     * @returns {number}
+     */
     static lowbit(x) {
         return x & -x;
     }
